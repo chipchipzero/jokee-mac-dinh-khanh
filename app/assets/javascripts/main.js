e0595979fd147c06790dde099a47fc806d8f49b2
Khánh Mai Đình <mr.khanh007@gmail.com>
2016-04-13 22:38:52 +0700

    Jokee: update js, css, controller to load joke and update vote

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,11 +1,59 @@
 $(function(){
 
     'use strict';
+    var votes = Cookies.getJSON('jokee_votes');
     $(window).on('load', function() {
         showJoke();
     });
 
+    $('.vote-button button').each(function() {
+      $(this).click(function() {
+        var temp = { id: $('#joke_id').val(), score: $(this).attr('vote-data') };
+        votes.push(temp);
+        Cookies.set('jokee_votes', JSON.stringify(votes));
+        showJoke();
+      });
+    });
+
     function showJoke() {
+      if(votes === undefined) {
+        votes = [];
+      }
       
+      $.ajax({
+        async: false,
+        method: 'post',
+        url: '/api/v1/api1',
+        data: {votes: votes},
+        dataType: 'json',
+        beforeSend: function() {
+          $('.vote-button button').each(function() {
+            $(this).attr('disabled', true);
+          });
+          $('.vote-button').hide();
+        },
+        success: function(res) {
+          if(res.status == 1) {
+            $('#joke_id').val(res.data.id);
+            $('.joke-content').removeClass('text-center').html(nl2br(res.data.content));
+            $('.vote-button button').each(function() {
+              $(this).removeAttr('disabled');
+            });
+            $('.vote-button').show();
+          } else if(res.status == 2) {
+            $('.joke-content').addClass('text-center').text(res.message);
+          } else {
+            alert("Error.");
+          }
+        },
+        error: function() {
+          alert("Error.");
+        }
+      });
+    }
+
+    function nl2br (str, is_xhtml) {   
+      var breakTag = (is_xhtml || typeof is_xhtml === 'undefined') ? '<br />' : '<br>';
+      return (str + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1'+ breakTag +'$2');
     }
 });
--- app/controllers/api/v1_controller.rb
+++ app/controllers/api/v1_controller.rb
@@ -1,19 +1,34 @@
 class Api::V1Controller < ApiController
   # @author: dinhkhanh
   # note : get one joke record by user
-  # param: {ids}
+  # param: {votes}
   def api1
     user = request.remote_ip
-    if Vote.where(user: user, voted_on: Date.today).empty?
-      if params[:ids].nil?
+    today = Date.today
+    if Vote.where(user: user, voted_on: today).empty?
+      if params[:votes].nil? || params[:votes].empty?
         joke = Joke.order('RAND()').first
       else
-        joke = Joke.where.not(id: params[:ids]).first
+        # render_success params[:votes] and return
+        ids = [];
+        params[:votes].each do | vote |
+          ids << vote[1][:id]
+        end
+        joke = Joke.where.not(id: ids).first
       end
 
-      render_success joke.as_json({only: [ :id, :content ]})
+      if joke.nil?
+        # save vote result
+        params[:votes].each do | vote |
+          Vote.create!(user: user, joke_id: vote[1][:id], vote: vote[1][:score], voted_on: today)
+        end
+        render_failed(2, "That's all the jokes for today! Come back another day!")
+      else
+        render_success joke.as_json({only: [ :id, :content ]})
+      end
     else
       render_failed(2, "That's all the jokes for today! Come back another day!")
     end
   end
+
 end
